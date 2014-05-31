c0394ff58b2f7bbcd7e96822dd23c8ea2e95cb59
19 <sorarize@gmail.com>
2014-05-31 15:57:05 +0800

    Clean head and footer

--- footer.php
+++ footer.php
@@ -1,11 +1,6 @@
-<div class="clear"></div>
-</div>
-<footer id="footer" role="contentinfo">
-<div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'mhouse' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'mhouse' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
-</div>
-</footer>
-</div>
+  </div><!-- end of #container -->
+  <footer id="footer"></footer>
+</div><!-- end of #wrapper -->
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -9,16 +9,12 @@
 </head>
 <body <?php body_class(); ?>>
 <div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<section id="branding">
-<div id="site-title"><?php if ( ! is_singular() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'mhouse' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( ! is_singular() ) { echo '</h1>'; } ?></div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-</section>
-<nav id="menu" role="navigation">
-<div id="search">
-<?php get_search_form(); ?>
-</div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
-</header>
-<div id="container">
+  <header id="header" class="clearfix" role="banner">
+    <h1 id="site-title">
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'mhouse' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
+    </h1>
+    <nav id="menu" role="navigation">
+      <?php wp_nav_menu(); ?>
+    </nav>
+  </header>
+  <div id="container">
